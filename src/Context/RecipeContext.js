0f30afacc00a6fd7b38f83db5260ef3298dfc35c
robn8 <rob.natale.8@gmail.com>
2023-01-01 13:16:46 -0600

    Added reducer to manage recipe context

--- src/Context/RecipeContext.js
+++ src/Context/RecipeContext.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 
 const RecipeContext = React.createContext();
 
+const recipeReducer = (state, action) => {
+    switch (action.type) {
+        case 'add_recipe': 
+            return [...state, { title: `Blog Post #${state.length + 1}` }];
+        default: 
+            return state;
+    }
+};
+
 export const RecipeProvider = ({ children }) => {
-    return <RecipeContext.Provider value={5}>{children}</RecipeContext.Provider>
+    const [recipePosts, dispatch] = useReducer(recipeReducer, []);
+
+    const addRecipePost = () => {
+        dispatch({ type: 'add_recipe' });
+    };
+
+    return (
+        <RecipeContext.Provider value={{ data: recipePosts, addRecipePost }}>
+            {children}
+        </RecipeContext.Provider>
+    );
 };
 
 export default RecipeContext;
--- src/Screens/IndexScreen.js
+++ src/Screens/IndexScreen.js
@@ -1,14 +1,21 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
 import RecipeContext from '../Context/RecipeContext';
 
 const IndexScreen = () => {
-    const value = useContext(RecipeContext);
+    const { data, addRecipePost } = useContext(RecipeContext);
 
     return (
         <View>
             <Text>Index Screen</Text>
-            <Text>{value}</Text>
+            <Button title="Add Recipe" onPress={addRecipePost} />
+            <FlatList 
+                data={data}
+                keyExtractor={(recipePost) => recipePost.title}
+                renderItem={({ item }) => {
+                    return <Text>{item.title}</Text>
+                }}
+            />
         </View>
     );
 };
