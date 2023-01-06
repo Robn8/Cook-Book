53ccb96ad96732448deef0d97d37b96bd2574f2e
robn8 <rob.natale.8@gmail.com>
2023-01-05 20:00:23 -0600

    Added Form to CreateScren component

--- src/Screens/CreateScreen.js
+++ src/Screens/CreateScreen.js
@@ -1,16 +1,46 @@
-import React, { useContext } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useContext, useState } from 'react';
+import { View, Text, TextInput, StyleSheet, Button } from 'react-native';
 import { Context } from '../Context/RecipeContext';
 
 const CreateScreen = ({ navigation }) => {
+  const [title, setTitle] = useState('');
+  const [ing, setIng] = useState('');  
 
   return (
     <View>
-      <Text>Create Recipe</Text>
+      <Text style={styles.label}>Recipe Name</Text>
+      <TextInput
+        style={styles.input}
+        value={title} 
+        onChangeText={(text) => setTitle(text)} 
+      />
+      <Text style={styles.label}>Ingredients needed</Text>
+      <TextInput
+        style={styles.input}
+        value={ing}
+        onChangeText={(text) => setIng(text)}
+      />
+      <Button 
+        title="Add New Recipe!"
+      />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    input: {
+        fontSize: 18,
+        borderWidth: 1,
+        borderColor: 'black',
+        marginBottom: 15,
+        padding: 5,
+        margin: 5
+    },
+    label: {
+        fontSize: 20,
+        marginBottom: 5,
+        marginLeft: 5
+    }
+});
 
 export default CreateScreen;
